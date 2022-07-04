dee8b6869112d8444de7c8be1805722c8e9d15e4
ritikmishra7 <96335947+ritikmishra7@users.noreply.github.com>
2022-07-04 12:53:18 +0530

    Update matrix_chain.cpp

--- matrix_chain.cpp
+++ matrix_chain.cpp
@@ -28,9 +28,11 @@ int MatrixChainOrder(int d[], int n)
 
 int main()
 {
-	int arr[] = { 1, 2, 3, 4 };
-	int size = sizeof(arr) / sizeof(arr[0]);
-
+	int size;
+	cout<<"Enter size of d array"<<endl;
+	cin>>size;
+	int arr[size];
+	for(int i=0; i<size; i++) cin>>arr[i];
 	cout << "Minimum number of multiplications is "
 		<< MatrixChainOrder(arr, size);
 
